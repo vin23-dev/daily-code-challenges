8f3cb865ea25510bf063aaabf5675ec9d6e2976b
Vincent Kokojan <vincentkokojan@vincents-mbp.attlocal.net>
2020-05-05 11:54:01 -0400

    roman to int

--- challenges.js
+++ challenges.js
@@ -177,39 +177,72 @@
 // ["HH", "HT", "TH", "TT"]
 // H stands for Heads and T stands for tails
 
-function coinFlips(n){
-    let outcomes = Array(Math.pow(2, n)).fill("");
-    let half = Math.pow(2, n) / 2;
-    let flipper = 1;
-    let counter = 0;
-    function flip(half){
-        for(let i = 0; i < Math.pow(2, n); i ++){
-            if(flipper === 1){
-                outcomes[i] += "H"
-            }else{
-                outcomes[i] += "T"
-            }
-            counter ++;
-            if(counter === half){
-                flipper *= -1;
-                counter = 0;
-            }
+// function coinFlips(n){
+//     let outcomes = Array(Math.pow(2, n)).fill("");
+//     let half = Math.pow(2, n) / 2;
+//     let flipper = 1;
+//     let counter = 0;
+//     function flip(half){
+//         for(let i = 0; i < Math.pow(2, n); i ++){
+//             if(flipper === 1){
+//                 outcomes[i] += "H"
+//             }else{
+//                 outcomes[i] += "T"
+//             }
+//             counter ++;
+//             if(counter === half){
+//                 flipper *= -1;
+//                 counter = 0;
+//             }
+//         }
+//         if(outcomes[0].length >= n){
+//             return outcomes;
+//         }
+//         return flip(half/2);
+//     } 
+//     return flip(half);
+// }
+
+// runA = coinFlips(2)
+// runB = coinFlips(3)
+// runC = coinFlips(4)
+// runD = coinFlips(20)
+// console.log(runA)
+// console.log(runB)
+// console.log(runC)
+// console.log(runD)
+
+
+
+
+
+
+
+
+// Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+var romanToInt = function(roman) {
+    value = { 
+            'M': 1000, 
+            'D': 500, 
+            'C': 100, 
+            'L': 50, 
+            'X': 10, 
+            'V': 5, 
+            'I': 1
         }
-        if(outcomes[0].length >= n){
-            return outcomes;
+    
+    number = 0;
+    
+    for (let i = 0; i < roman.length; i++){
+        current = value[roman[i]];
+        next = value[roman[i + 1]];
+        if (current < next){
+            number += next - current;
+            i++;
+        }else{
+            number += current;
         }
-        return flip(half/2);
-    } 
-    return flip(half);
-}
-
-runA = coinFlips(2)
-runB = coinFlips(3)
-runC = coinFlips(4)
-runD = coinFlips(20)
-console.log(runA)
-console.log(runB)
-console.log(runC)
-console.log(runD)
-
-
+    }   
+    return number;
+};
